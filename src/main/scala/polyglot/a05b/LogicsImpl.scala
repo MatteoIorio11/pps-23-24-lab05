1df4322938911df37720aa8a8a9864608fab1864
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-08 16:34:52 +0200

    feat: refactor of the entire class

--- src/main/scala/polyglot/a05b/LogicsImpl.scala
+++ src/main/scala/polyglot/a05b/LogicsImpl.scala
@@ -6,28 +6,21 @@ import polyglot.a05b.Grid
 import util.Sequences.Sequence
 import util.Sequences.Sequence.empty
 
+import scala.util.Random
+
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a05b/sol2/ */
 class LogicsImpl(private val size: Int) extends Logics:
+  lazy val random: Random = Random()
+  private var tickCounter: Int = 0
+  private var initial: Pair[Int, Int] = Pair(random.nextInt(this.size - 2) + 1, random.nextInt(this.size - 2) + 1)
 
-  private val grid: Grid = GridImpl(size, 5)
-  private val clickedCells: Sequence[Pair[Int, Int]] = Sequence.empty
-  private val directions: Sequence[Pair[Int, Int]] = Sequence(
-    Pair(-1, -1),
-    Pair(-1, 0),
-    Pair(-1, 1),
-    Pair(0, -1),
-    Pair(0, 1),
-    Pair(1, 0),
-    Pair(1, 1))
-
-  override def tick(): Unit = {}
+  override def tick(): Unit =
+    this.tickCounter = this.tickCounter + 1
 
   override def isOver: Boolean = false
 
-  override def hasElement(x: Int, y: Int): Boolean =
-    if x < 0 || y < 0 || x >= this.size || y >= this.size then
-      throw IllegalStateException("Coordinate outside the grid.")
-    else
-      !this.grid.getBombs.find(el => el.x == x && el.y == y).isEmpty
+
+  override def hasElement(x: Int, y: Int): Boolean = false
+
 
 
