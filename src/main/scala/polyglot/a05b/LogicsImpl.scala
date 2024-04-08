80a4fe9c0693aaf665489bd1a930dd071a8b79fd
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-08 11:24:22 +0200

    feat: init LogicsImpl

--- src/main/scala/polyglot/a05b/LogicsImpl.scala
+++ src/main/scala/polyglot/a05b/LogicsImpl.scala
@@ -1,11 +1,19 @@
 package polyglot.a05b
 
+import polyglot.Pair
 import polyglot.a05b.Logics
+import polyglot.a05b.Grid
 
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a05b/sol2/ */
 class LogicsImpl(private val size: Int) extends Logics:
+
+  private val grid: Grid = GridImpl(size, 5)
+  private val clickedCells: Sequence[Pair[Int, Int]] = Sequence.empty()
+
   override def tick(): Unit = {}
 
   override def isOver: Boolean = false
 
-  override def hasElement(x: Int, y: Int): Boolean = false
+  override def hasElement(x: Int, y: Int): Boolean =
+
+
