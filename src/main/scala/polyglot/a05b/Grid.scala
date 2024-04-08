919c8fa7ac502c3ce111e5f40aa95d9137a6bc20
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-08 11:54:04 +0200

    refactor: use find instead of contains

--- src/main/scala/polyglot/a05b/Grid.scala
+++ src/main/scala/polyglot/a05b/Grid.scala
@@ -1,6 +1,5 @@
 package polyglot.a05b
 
-import util.GridElementImpl
 import util.Optionals.Optional
 import util.Optionals.Optional.Just
 import util.Sequences.Sequence
@@ -19,15 +18,15 @@ class GridImpl(val size: Int, val nBombs: Int) extends Grid:
   private def initializeGrid(): Unit =
     (0 until size).foreach(row =>
       (0 to size).foreach(col =>
-        if !this.grid.contains(grdEl => grdEl.x == row && grdEl.y == col) then
-          this.grid.concat(Sequence(GridElementImpl(row, col, false)))))
+        if this.grid.find(grdEl => grdEl.x == row && grdEl.y == col).isEmpty then
+          this.grid = this.grid.concat(Sequence(new GridElementImpl(row, col, false)))))
   private def spawnBombs(size: Int, bombs: Int): Unit =
     var spawnedBombs = 0
     var random = Random()
     while (spawnedBombs < bombs)
-      val possibleBomb = GridElementImpl(random.nextInt(size-1), random.nextInt(size-1), true)
-      if (!this.grid.contains(gridEl => (gridEl.x == possibleBomb.x && gridEl.y == possibleBomb.y && gridEl.bomb == false))) then
-        this.grid.concat(Sequence(possibleBomb))
+      val possibleBomb = new GridElementImpl(random.nextInt(size-1), random.nextInt(size-1), true)
+      if this.grid.find(gridEl => gridEl.x == possibleBomb.x && gridEl.y == possibleBomb.y && !gridEl.bomb).isEmpty then
+        this.grid = this.grid.concat(Sequence(possibleBomb))
         spawnedBombs = spawnedBombs + 1
 
   override def checkBomb(x: Int, y: Int): Boolean =
@@ -37,5 +36,5 @@ class GridImpl(val size: Int, val nBombs: Int) extends Grid:
   override def getBombs: Sequence[GridElementImpl] =
     this.grid.filter(grdEl => grdEl.bomb)
 
-
+class GridElementImpl(val x: Int, val y: Int, val bomb: Boolean)
 
--- src/main/scala/util/GridElementImpl.scala
+++
@@ -1,3 +0,0 @@
-package util
-
-class GridElementImpl(val x: Int, val y: Int, val bomb: Boolean)
