843d63f9592903ac82cd0c6035bca6d7dab1f330
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-08 11:05:03 +0200

    feat: implement initializeGrid

--- src/main/scala/polyglot/a05b/Grid.scala
+++ src/main/scala/polyglot/a05b/Grid.scala
@@ -1,13 +1,21 @@
 package polyglot.a05b
 
-import util.Pair
+import util.GridElement
 import util.Sequences.Sequence
 
 
 trait Grid:
   def spawnBombs(size: Int, bombs: Int): Unit
-  def getBombs: Sequence[Pair]
+  def getBombs: Sequence[GridElement]
 
 class GridImpl(val size: Int, val nBombs: Int) extends Grid:
+  var grid: Sequence[GridElement] = Sequence.empty
+  this.initializeGrid()
+  private def initializeGrid(): Unit =
+    (0 until size).foreach(row =>
+      (0 to size).foreach(col =>
+        this.grid.concat(Sequence(GridElement(row, col, false)))))
+  override def spawnBombs(size: Int, bombs: Int): Unit = ???
+  override def getBombs: Sequence[GridElement] = ???
 
 
