b9ba52b39c7fe347b78c29b14d010eac1bb9765e
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-08 11:24:27 +0200

    refactor

--- src/main/scala/util/GridElementImpl.scala
+++ src/main/scala/util/GridElementImpl.scala
@@ -1,6 +1,3 @@
 package util
 
-class GridElement(val x: Int, val y: Int, val bomb: Boolean)
-
-object GridElement:
-  def apply(x: Int, y: Int, bomb: Boolean): GridElement = GridElement(x, y, bomb)
+class GridElementImpl(val x: Int, val y: Int, val bomb: Boolean)
