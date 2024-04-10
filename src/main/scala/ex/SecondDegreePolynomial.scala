1da811d6b4e844335c2a9a8de0b76271ef9a48fe
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-10 11:07:28 +0200

    feat: refactor class

--- src/main/scala/ex/SecondDegreePolynomial.scala
+++ src/main/scala/ex/SecondDegreePolynomial.scala
@@ -2,29 +2,30 @@ package ex
 
 import scala.annotation.targetName
 
-// Express a second degree polynomial
-// Structure: secondDegree * X^2 + firstDegree * X + constant
-trait SecondDegreePolynomial:
-  def constant: Double
-  def firstDegree: Double
-  def secondDegree: Double
-  def +(polynomial: SecondDegreePolynomial): SecondDegreePolynomial
-  def -(polynomial: SecondDegreePolynomial): SecondDegreePolynomial
-
-case class SecondDegreePolynomialImpl(val secondDegree: Double, val firstDegree: Double, val constant: Double) extends SecondDegreePolynomial:
-  override def -(polynomial: SecondDegreePolynomial): SecondDegreePolynomial =
-    SecondDegreePolynomial.createSecondDegreeFromOperation(this, polynomial)((x1, x2) => x1 - x2)
-
-
-  override def +(polynomial: SecondDegreePolynomial): SecondDegreePolynomial =
-    SecondDegreePolynomial.createSecondDegreeFromOperation(this, polynomial)((x1, x2) => x1 + x2)
-
 object SecondDegreePolynomial:
   def apply(secondDegree: Double, firstDegree: Double, constant: Double): SecondDegreePolynomial = SecondDegreePolynomialImpl(secondDegree, firstDegree, constant)
-  def createSecondDegreeFromOperation(pl1: SecondDegreePolynomial, pl2: SecondDegreePolynomial)(f: (Double, Double) => Double): SecondDegreePolynomial =
+  private def createSecondDegreeFromOperation(pl1: SecondDegreePolynomial, pl2: SecondDegreePolynomial)(f: (Double, Double) => Double): SecondDegreePolynomial =
     SecondDegreePolynomialImpl(f(pl1.secondDegree, pl2.secondDegree),
       f(pl1.firstDegree, pl2.firstDegree),
       f(pl1.constant, pl2.constant))
+
+// Express a second degree polynomial
+// Structure: secondDegree * X^2 + firstDegree * X + constant
+  trait SecondDegreePolynomial:
+    def constant: Double
+    def firstDegree: Double
+    def secondDegree: Double
+    def +(polynomial: SecondDegreePolynomial): SecondDegreePolynomial
+    def -(polynomial: SecondDegreePolynomial): SecondDegreePolynomial
+
+  case class SecondDegreePolynomialImpl(val secondDegree: Double, val firstDegree: Double, val constant: Double) extends SecondDegreePolynomial:
+    override def -(polynomial: SecondDegreePolynomial): SecondDegreePolynomial =
+      SecondDegreePolynomial.createSecondDegreeFromOperation(this, polynomial)((x1, x2) => x1 - x2)
+
+    override def +(polynomial: SecondDegreePolynomial): SecondDegreePolynomial =
+      SecondDegreePolynomial.createSecondDegreeFromOperation(this, polynomial)((x1, x2) => x1 + x2)
+
+
 
 @main def checkComplex(): Unit =
   val simplePolynomial = SecondDegreePolynomial(1.0, 0, 3)
